35beff65e03a4cdb2efd32c1f47b148caebda457
leducanh1508 <bokholeki@gmail.com>
2025-04-25 16:02:35 +0700

    deploy

--- src/utils/services/Api.ts
+++ src/utils/services/Api.ts
@@ -262,7 +262,7 @@ export class HttpClient<SecurityDataType = unknown> {
   }: ApiConfig<SecurityDataType> = {}) {
     this.instance = axios.create({
       ...axiosConfig,
-      baseURL: axiosConfig.baseURL || "http://localhost:8080",
+      baseURL: axiosConfig.baseURL || "https://drink-shop-be.onrender.com",
     });
     this.secure = secure;
     this.format = format;
